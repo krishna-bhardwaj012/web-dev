61d0a690c8d67f1b49b41dff185b99d8f013d043
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-11-30 21:06:53 +0530

    Update

--- bootcamp/JavaScript/05_string-methods-and-properties/script.js
+++ bootcamp/JavaScript/05_string-methods-and-properties/script.js
@@ -8,3 +8,17 @@ const lastFourDigits = '7763'
 const maskedAccountNumber = lastFourDigits.padStart(16, '*')
 
 const capitalMessage = message.toUpperCase()
+
+const bankBalance = 9873
+
+// const templateString = `Last four digit of my account number is ` + lastFourDigits
+
+// const templateString = `Last four digit of my account number is `.concat(lastFourDigits) 
+
+const concatenatedString = 'Last four digit of my account number is '.concat(' ', lastFourDigits)
+
+// const templateString = `Last four digit of my account number is ${lastFourDigits}`
+
+const templateString = `Last four digit of my account number is ${lastFourDigits.padStart(16, '*')}.`
+
+const templateString2 = `I have ₹${bankBalance} in my account`
