afce4a7472d4108a044435ec0a32263539ddcaf7
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-11-30 19:59:16 +0530

    Update

--- bootcamp/JavaScript/05_string-methods-and-properties/script.js
+++ bootcamp/JavaScript/05_string-methods-and-properties/script.js
@@ -1,7 +1,10 @@
-const message = 'Hello'
+const message = 'Hello World!'
 const faltuMessage = '          Hi, I am Krishna.      '
 const finalMessage = faltuMessage.trim()
 const finalMessageLowerCase = faltuMessage.trim().toLocaleLowerCase()
+const replacedMessage = finalMessage.replace('Hi', 'Hello')
 
+const lastFourDigits = '7763'
+const maskedAccountNumber = lastFourDigits.padStart(16, '*')
 
 const capitalMessage = message.toUpperCase()
