03e8231a4c501e73c400c5376b4d93797cc42e35
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-11-30 11:41:57 +0530

    String-Methods

---
+++ bootcamp/JavaScript/05_string-methods-and-properties/script.js
@@ -0,0 +1,7 @@
+const message = 'Hello'
+const faltuMessage = '          Hi, I am Krishna.      '
+const finalMessage = faltuMessage.trim()
+const finalMessageLowerCase = faltuMessage.trim().toLocaleLowerCase()
+
+
+const capitalMessage = message.toUpperCase()
