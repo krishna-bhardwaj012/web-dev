1ff52eae0c0789fb61404d5dcbdb93a2288f4eff
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-12-02 00:20:46 +0530

    decision-making

--- bootcamp/JavaScript/10_decision-making-in-javascript/script.js
+++ bootcamp/JavaScript/10_decision-making-in-javascript/script.js
@@ -1,17 +1,42 @@
-const username = prompt('Enter your name: ');
-const userAge = +prompt('Enter your age: ');
+let username = prompt('Enter your name: ')|| 'Pro Coder';
+let userAge = +prompt('Enter your age: ') || 22;
+
+// if (!username) {
+//     username = 'Pro Coder';
+// }
+
+// if (!userAge || isNaN(userAge)) {
+//     userAge = 22;
+// }
 
 console.log(`Name: ${username}`);
 console.log(`Age: ${userAge}`);
 
+
+
+if(userAge >= 0 && userAge <= 4) {
+    console.log(`${username} is a kid.`);
+    console.log('And he/she is playing.');
+}
+
+if(userAge >= 5 && userAge <= 17) {
+    console.log(`${username} is a school student.`);
+    console.log('And he/she is learning science and maths.');
+}
+
 if(userAge >= 18 && userAge <= 24) {
-    console.log('user is a college student.');
+    console.log(`${username} is a college student.`);
     console.log('And he/she is learning JavaScript.');
 }
 
 if(userAge >= 25 && userAge <= 45) {
-    console.log('user is a working professional');
+    console.log(`${username} is a working professional.` );
     console.log('And he/she is working in It.');
+}
+    
+if(userAge > 45) {
+    console.log(`${username} is a retired.`);
+    console.log('And he/she is reads newspaper.');
 }
     
 
