97d9c54b60f3aa965eb7484b4e1cc4c20025160f
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-12-01 21:00:42 +0530

    Logical-Operators-update

--- bootcamp/JavaScript/09_logical-operators/script.js
+++ bootcamp/JavaScript/09_logical-operators/script.js
@@ -8,3 +8,15 @@ const isStudent = isSchoolStudent || isCollegeStudent
 
 const andResult = 0 && 2
 const orResult = 0 || 2
+
+const andResult2 = '' && 'Hello'
+const orResult2 = '' || 'Hello'
+
+const andResult3 = 'Hello' && 'null'
+const orResult3 = 'Hello' || 'null'
+
+const andResult4 = 'Hello' && 4 + 8
+const orResult4 = 'Hello' || 4 + 8
+
+const andResult5 = 'Hello' && console.log('hello')
+const orResult5 = undefined || console.log('hello2')
