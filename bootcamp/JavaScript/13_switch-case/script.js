03aeb2c9c30db236d0e55bd457eb904b9bf6d85e
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-12-02 22:36:58 +0530

    update

--- bootcamp/JavaScript/13_switch-case/script.js
+++ bootcamp/JavaScript/13_switch-case/script.js
@@ -19,31 +19,57 @@
 
 const dayNumber = 14
 
-switch (dayNumber){
-    case 0:
-        console.log ('it is Sunday today')
-        break
-    case 1:
-        console.log ('it is Monday today')
-        break
-    case 2:
-        console.log ('it is Tuesday today')
-        break
-    case 3:
-        console.log ('it is Wednesday today')
-        break
-    case 4:
-        console.log ('it is Thursday today')
-        break
-    case 5:
-        console.log('it is Friday today')
-        break
-    case 6:
-        console.log('it is Saturday today')
-        break
-    default:
-        console.log('Please Enter a Valid Day Number')
+// switch (dayNumber){
+//     case 0:
+//         console.log ('it is Sunday today')
+//         break
+//     case 1:
+//         console.log ('it is Monday today')
+//         break
+//     case 2:
+//         console.log ('it is Tuesday today')
+//         break
+//     case 3:
+//         console.log ('it is Wednesday today')
+//         break
+//     case 4:
+//         console.log ('it is Thursday today')
+//         break
+//     case 5:
+//         console.log('it is Friday today')
+//         break
+//     case 6:
+//         console.log('it is Saturday today')
+//         break
+//     default:
+//         console.log('Please Enter a Valid Day Number')
 
+// }
+
+const username ='krishna'
+const userAge = 22
+
+
+if (userAge >= 0 && userAge <= 4) {
+    console.log(`${username} is a kid.`);
+    console.log('And he/she is playing.');
+} else if (userAge >= 5 && userAge <= 17) {
+    console.log(`${username} is a school student.`);
+    console.log('And he/she is learning science and maths.');
+} else if (userAge >= 18 && userAge <= 24) {
+    console.log(`${username} is a college student.`);
+    console.log('And he/she is learning computer science.');
+} else if (userAge >= 25 && userAge <= 45) {
+    console.log(`${username} is a working professional.`);
+    console.log('And he/she is a web developer.');
+} else if (userAge > 45 && userAge < 121) {
+    console.log(`${username} is retired.`);
+    console.log('And he/she reads newspaper.');
+} else if (userAge >= 121) {
+    console.log(`${username} is immortal.`);
+    console.log('And he/she reads newspaper.');
+} else {
+    console.log('Please Enter a Valid Age');
 }
 
 console.log('Program Ended!');
