809703c4703a57e9a00cf9c3165237a8a4e8633b
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-12-02 22:16:56 +0530

    Switch case

---
+++ bootcamp/JavaScript/13_switch-case/script.js
@@ -0,0 +1,23 @@
+const dayNumber = 3
+
+if (dayNumber === 0){
+    console.log ('it is Sunday')
+} else if(dayNumber === 1) {
+    console.log ('it is Monday')
+} else if (dayNumber === 2){
+    console.log ('it is Tuesday')
+} else if (dayNumber === 3){
+    console.log ('it is Wednesday')
+} else if (dayNumber === 4){
+    console.log ('it is Thursday')
+} else if (dayNumber === 5){
+    console.log ('it is Friday')
+} else if (dayNumber === 6){
+    console.log ('it is Saturday')
+} else {
+    console.log ('Invalid day number')
+}
+
+
+
+
