18f3bcb8c5941a9cc5f1389db586b4d63f16c8d4
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-12-02 22:30:21 +0530

    Switch case

--- bootcamp/JavaScript/13_switch-case/script.js
+++ bootcamp/JavaScript/13_switch-case/script.js
@@ -1,23 +1,52 @@
-const dayNumber = 3
 
-if (dayNumber === 0){
-    console.log ('it is Sunday')
-} else if(dayNumber === 1) {
-    console.log ('it is Monday')
-} else if (dayNumber === 2){
-    console.log ('it is Tuesday')
-} else if (dayNumber === 3){
-    console.log ('it is Wednesday')
-} else if (dayNumber === 4){
-    console.log ('it is Thursday')
-} else if (dayNumber === 5){
-    console.log ('it is Friday')
-} else if (dayNumber === 6){
-    console.log ('it is Saturday')
-} else {
-    console.log ('Invalid day number')
+// if (dayNumber === 0){
+//     console.log ('it is Sunday')
+// } else if(dayNumber === 1) {
+//     console.log ('it is Monday')
+// } else if (dayNumber === 2){
+//     console.log ('it is Tuesday')
+// } else if (dayNumber === 3){
+//     console.log ('it is Wednesday')
+// } else if (dayNumber === 4){
+//     console.log ('it is Thursday')
+// } else if (dayNumber === 5){
+//     console.log ('it is Friday')
+// } else if (dayNumber === 6){
+//     console.log ('it is Saturday')
+// } else {
+//     console.log ('Please Enter a Valid Day Number')
+// }
+
+const dayNumber = 14
+
+switch (dayNumber){
+    case 0:
+        console.log ('it is Sunday today')
+        break
+    case 1:
+        console.log ('it is Monday today')
+        break
+    case 2:
+        console.log ('it is Tuesday today')
+        break
+    case 3:
+        console.log ('it is Wednesday today')
+        break
+    case 4:
+        console.log ('it is Thursday today')
+        break
+    case 5:
+        console.log('it is Friday today')
+        break
+    case 6:
+        console.log('it is Saturday today')
+        break
+    default:
+        console.log('Please Enter a Valid Day Number')
+
 }
 
+console.log('Program Ended!');
 
 
 
