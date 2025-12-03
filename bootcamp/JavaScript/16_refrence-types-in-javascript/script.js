07149631ab06667c20aa29a58532783c34712674
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-12-04 00:25:51 +0530

    refrence-types

---
+++ bootcamp/JavaScript/16_refrence-types-in-javascript/script.js
@@ -0,0 +1,45 @@
+// const user = {firstName: 'akash',
+//     lastName: 'kumar',
+//     age: 26,
+//     education: 12,
+//     city: 'bangalore'
+// }
+
+const myName = 'krishna'
+
+const username1 = ''
+const username2 = ''
+
+const user1 ={
+    firstName: 'sachin'
+
+}
+
+// const user2 ={
+//     firstName: 'adarsh',
+//     'last-Name': 'singh'
+
+// }
+
+// console.log(user2.lastName);
+// console.log(user2["lastName"]);
+// console.log(user2[myName]);
+// console.log(user2["first" + "Name"]);
+
+
+const user2 ={
+    firstName: 'adarsh',
+    lastName: 'singh',
+    pata:{
+        city: 'delhi',
+        pinCode: 110001,
+        state: 'delhi',
+        moreDetails: {
+            population: 7890659006,
+            area: 2500
+        }
+    }
+}
+
+// user2.age = 22
+// user["is-student"] = true
