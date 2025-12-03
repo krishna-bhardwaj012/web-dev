49f926d78bec7da5720a3994948298a709764b61
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-12-03 21:49:52 +0530

    update

---
+++ bootcamp/JavaScript/14_ternary-operator/script.js
@@ -0,0 +1,11 @@
+// const username = 5 > 2 ? 'Rohit' : 'Player'
+
+// const gender = 'F'
+
+// const userMessage =` ${gender.toLocaleLowerCase() === 'f' ? 'She' : 'He' } is a college student.`
+
+// console.log(userMessage)
+
+const result = null ? 'Sachin' : 'Bhardwaj' ? '12' : 0
+
+console.log(result)
