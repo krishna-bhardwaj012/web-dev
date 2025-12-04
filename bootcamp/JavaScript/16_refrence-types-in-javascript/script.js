3d1a9eaf4de49fea719da6ce5eafd5386399726c
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-12-04 23:19:22 +0530

    refrence-types-update

--- bootcamp/JavaScript/16_refrence-types-in-javascript/script.js
+++ bootcamp/JavaScript/16_refrence-types-in-javascript/script.js
@@ -1,45 +1,62 @@
-// const user = {firstName: 'akash',
-//     lastName: 'kumar',
-//     age: 26,
-//     education: 12,
-//     city: 'bangalore'
+// // const user = {firstName: 'akash',
+// //     lastName: 'kumar',
+// //     age: 26,
+// //     education: 12,
+// //     city: 'bangalore'
+// // }
+
+// const myName = 'krishna'
+
+// const username1 = ''
+// const username2 = ''
+
+// const user1 ={
+//     firstName: 'sachin'
+
 // }
+
+// const user2 ={
+//     firstName: 'adarsh',
+//     'last-Name': 'singh'
+
+// // }
+
+// // console.log(user2.lastName);
+// // console.log(user2["lastName"]);
+// // console.log(user2[myName]);
+// // console.log(user2["first" + "Name"]);
+
+// const user2 ={
+//     firstName: 'adarsh',
+//     lastName: 'singh',
+//     pata:{
+//         city: 'delhi',
+//         pinCode: 110001,
+//         state: 'delhi',
+//         moreDetails: {
+//             population: 7890659006,
+//             area: 2500
+//         }
+//     }
+// }
+
+// // user2.age = 22
+// // user["is-student"] = true
 
 const myName = 'krishna'
 
 const username1 = ''
 const username2 = ''
 
-const user1 ={
+const user1 = {
     firstName: 'sachin'
-
+}
+const user2 = {
+    firstName: 'krishna',
+    lastName : 'Bhardwaj',
+    krishna: 'developer'
 }
 
-// const user2 ={
-//     firstName: 'adarsh',
-//     'last-Name': 'singh'
-
-// }
-
-// console.log(user2.lastName);
-// console.log(user2["lastName"]);
-// console.log(user2[myName]);
-// console.log(user2["first" + "Name"]);
-
-
-const user2 ={
-    firstName: 'adarsh',
-    lastName: 'singh',
-    pata:{
-        city: 'delhi',
-        pinCode: 110001,
-        state: 'delhi',
-        moreDetails: {
-            population: 7890659006,
-            area: 2500
-        }
-    }
-}
-
-// user2.age = 22
-// user["is-student"] = true
+console.log(user2.firstName);
+console.log(user2["lastName"]);
+console.log(user2.krishna);
