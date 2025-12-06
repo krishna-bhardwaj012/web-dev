b6f14ee4bc33a8bb676cc65e596327d4698d438a
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-12-07 01:03:16 +0530

    Multidimensional Array

---
+++ bootcamp/JavaScript/20_multidimentional-array/script.js
@@ -0,0 +1,11 @@
+const nameAndNumberList = [
+    ['Adarsh', 75],
+    ['Brenda', 82],
+    ['Carlos', 90],
+]
+
+const ticTacToe = [
+    ['X', null, null],
+    [null, null, 'O'],
+    ['O',null, 'X']
+]
