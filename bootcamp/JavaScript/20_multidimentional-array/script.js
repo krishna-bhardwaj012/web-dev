19b6f5d756cf18dbe69ce46c8f3ce0cc54f53d8f
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-12-09 23:15:22 +0530

    update

--- bootcamp/JavaScript/20_multidimentional-array/script.js
+++ bootcamp/JavaScript/20_multidimentional-array/script.js
@@ -8,4 +8,19 @@ const ticTacToe = [
     ['X', null, null],
     [null, null, 'O'],
     ['O',null, 'X']
+
 ]
+
+console.log(nameAndNumberList[1][0]); // Brenda
+console.log(ticTacToe[2][2]); // X
+console.log(ticTacToe[0][0]); // X
+console.log(nameAndNumberList[2][1]); // 90
+console.log(ticTacToe[1][2]); // O
+console.log(nameAndNumberList[0][1]); // 75
+console.log(ticTacToe[2][0]); // O
+console.log(nameAndNumberList[1][1]); // 82
+console.log(ticTacToe[0][1]); // null
+console.log(nameAndNumberList[2][0]); // Carlos
+console.log(ticTacToe[1][1]); // null
+console.log(nameAndNumberList[0][0]); // Adarsh
+console.log(ticTacToe[0][2]); // null
