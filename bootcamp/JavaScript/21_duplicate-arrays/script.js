e4c3d4aadb45f53d220c39b329536ee703a6c9c1
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-12-11 00:37:53 +0530

    update

---
+++ bootcamp/JavaScript/21_duplicate-arrays/script.js
@@ -0,0 +1,23 @@
+// const username1 = 'Krishna'
+
+// let username2 = username1
+
+// username2 = username2 + ' Kumar'
+
+const fruits = ['Mango', 'Apple', 'Orange']
+
+const myFruits = fruits
+
+myFruits.push('Dates')
+myFruits.push('Grapes')
+
+const user1 = {
+    firstName: 'Krishna',
+    lastName: 'Kumar'
+}
+
+const user2 = {}
+
+user2.lastName = ()
+
+
