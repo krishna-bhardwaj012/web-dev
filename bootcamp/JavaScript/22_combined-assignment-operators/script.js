7093e42c4ea69601c9d317fa341a1254c8a29ba7
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-12-12 22:30:34 +0530

    Combined Assignment Operator

---
+++ bootcamp/JavaScript/22_combined-assignment-operators/script.js
@@ -0,0 +1,34 @@
+let num = 3
+
+// num = num + 2
+// num = num + 2
+// num = num + 2
+// num = num + 2
+// num = num + 2
+
+// num += 5
+// num += 5
+// num += 5
+// num += 5
+
+
+// num *= 5
+// num *= 5
+// num *= 5
+// num *= 5
+
+
+// num **= 5
+// num **= 5
+// num **= 5
+// num **= 5
+
+// num ++ // num ++ increases num by 1 after its current value is used
+// num ++ 
+// num ++ 
+// num ++ 
+
+// ++ num // ++ num increases num by 1 before its current value is used
+// ++ num 
+// ++ num 
+// ++ num 
