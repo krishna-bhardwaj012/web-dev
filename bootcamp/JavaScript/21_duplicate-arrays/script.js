7a002bfbf9f653cd06a566eaf6582a886423b8e9
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-12-12 21:44:52 +0530

    update

--- bootcamp/JavaScript/21_duplicate-arrays/script.js
+++ bootcamp/JavaScript/21_duplicate-arrays/script.js
@@ -6,18 +6,35 @@
 
 const fruits = ['Mango', 'Apple', 'Orange']
 
-const myFruits = fruits
+// const myFruits = fruits
 
-myFruits.push('Dates')
-myFruits.push('Grapes')
+// myFruits.push('Dates')
+// myFruits.push('Grapes')
+
+
+
+// Object.assign(myFruits, fruits) // create new array by Object.assign
+const myFruits = [...fruits] // create new array by spread operator
+
+
+// const myFruits =[].concat(fruits) // create new array by concatenation
+
+// const myFruits = fruits.slice() // create new array by slicing all elements
 
 const user1 = {
     firstName: 'Krishna',
-    lastName: 'Kumar'
+    lastName: 'Kumar',
+    pata: {
+        city: 'Bangalore',
+        pincode: 560037
+    }
 }
 
-const user2 = {}
+// const user2 = {}
+// Object.assign(user2, user1)
 
-user2.lastName = ()
+// user2.lastName = ()
+
+const user2 = {...user1}
 
 
