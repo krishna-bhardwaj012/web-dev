a2cd6cab56cf3a138470b0c63b67d9b29be44d26
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-12-14 23:48:16 +0530

    while-loop-update

--- bootcamp/JavaScript/23_while-loop/script.js
+++ bootcamp/JavaScript/23_while-loop/script.js
@@ -9,10 +9,10 @@ console.log('Program Started')
 //     num = num + 1
 // }
 
-const friends = ['Akash', 'Rishi', 'Ritik', 'Setu', 'Nitish']
+const friends = ['Akash', 'Rishi', 'Ritik', 'Setu', 'Nitish', 'Sumit']
 let i = 0
-while(i <= friends.length){
-    console.log('friends[i]');
+while(i < friends.length){
+    console.log(`${i + 1}. ${friends[i]}`);
     i++
 }
 
