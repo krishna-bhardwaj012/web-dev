ab931a68a8a9d13031cb6c1ad23f7a3cf383fe05
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-12-15 23:48:26 +0530

    for-loop

---
+++ bootcamp/JavaScript/24_for-loop/script.js
@@ -0,0 +1,13 @@
+console.log('Program Start')
+
+
+
+// for(let i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+for(let i = 0; i < 10; i++){
+    console.log(i + 1);
+}
+
+console.log('Program Ended');
