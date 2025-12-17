f8d6ef65524a1bcbbdbeac26c56b0a33e739e8f4
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-12-17 22:01:40 +0530

    Do-while-loop-update

---
+++ bootcamp/JavaScript/25_do-while-loop/script.js
@@ -0,0 +1,21 @@
+console.log('Program Start')
+
+// let i = 0 
+// while (i < 5) {
+//     console.log(i)
+//     i++
+// }
+
+
+// for (let i = 10; i < 5; i++) {
+//     console.log(i);
+// }
+
+
+let i = 10
+do {
+    console.log(i);
+    i++
+}while (i < 5)
+
+console.log('Program Ended')
