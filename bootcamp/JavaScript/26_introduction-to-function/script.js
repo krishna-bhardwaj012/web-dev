02400281df290ba5b06083c665727573ff482861
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-12-19 00:01:35 +0530

    Introduction-to-function

---
+++ bootcamp/JavaScript/26_introduction-to-function/script.js
@@ -0,0 +1,14 @@
+// Function Definition 
+function introduceMe() {
+   console.log('Hi,');
+   console.log('My name is sachin.');
+   console.log('I am a web developer.');
+}
+
+//                  Function call
+// const returnValue = introduceMe()
+
+introduceMe()
+console.log('*******************');
+introduceMe()
+console.log('******************')
