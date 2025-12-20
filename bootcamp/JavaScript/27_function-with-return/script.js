209bf508283de943772c55814fceb4ba0acddde4
Krishna Bhardwaj <sachinkumar221155@gmail.com>
2025-12-20 22:07:52 +0530

    Function-with-return

---
+++ bootcamp/JavaScript/27_function-with-return/script.js
@@ -0,0 +1,18 @@
+function addTwoNumbers(a, b) {
+
+    return  a + b
+}
+
+const result = addTwoNumbers(5, 6)
+
+console.log(result);
+
+
+function subtractTwoNumbers(a, b) {
+    return a - b
+}
+ 
+const results = subtractTwoNumbers(10, 4)
+
+console.log(results);
+
